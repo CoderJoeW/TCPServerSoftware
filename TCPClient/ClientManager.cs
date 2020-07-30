a169f11c84ef2474b08a613ac878596710ee05df
Joseph Williamson <j.williamson@gambler-labs.com>
2020-07-30 17:02:27 -0400

    Addition of unity specific classes

---
+++ TCPClient/ClientManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientManager : Singleton<ClientManager> {
+	[SerializeField]
+	private string ipAddress;
+
+	[SerializeField]
+	private int port;
+
+    [SerializeField]
+    private string clientVersion;
+
+	private void Awake(){
+		DontDestroyOnLoad (this);
+		UnityThread.initUnityThread ();
+
+		ClientHandleData.InitializePacketListener ();
+		ClientTCP.InitializeClientSocket (ipAddress, port);
+	}
+
+    public string GetClientVersion() {
+        return clientVersion;
+    }
+
+    private void OnApplicationQuit() {
+        ClientTCP.PACKET_DisconnectFromServer();
+    }
+
+    private void OnApplicationPause() {
+        ClientTCP.PACKET_DisconnectFromServer();
+    }
+}
